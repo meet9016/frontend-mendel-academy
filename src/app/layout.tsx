739d89e4ad50aa59497a235937fceb9a26aef20b
meet <meet.shopno@gmail.com>
2025-11-21 10:24:10 +0530

    header and footer layout file set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Toastify from "@/comman/Toastify";
+import Header from "@/component/auth/Header";
+import Footer from "@/component/auth/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,8 +40,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
           <Toastify />
+          <Footer />
       </body>
     </html>
   );
