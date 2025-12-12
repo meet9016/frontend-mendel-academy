ff0999721db903777f49cef4ed525a1bfdd601fc
meet <meet.shopno@gmail.com>
2025-12-12 11:32:21 +0530

    base url set dynamic

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,42 +1,42 @@
 "use client";
-import axios from 'axios'
+import axios from "axios";
 
 // const baseURL = process.env.NEXT_PUBLIC_APP_URL;
-const baseURL = 'https://christeen-unmaidenlike-bloomingly.ngrok-free.dev/api/v1/';
-
+const baseURL =
+  "https://christeen-unmaidenlike-bloomingly.ngrok-free.dev/api/v1/";
 
 const apiAdminInstance = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   // headers: {
   //   'Content-Type': 'multipart/form-data'
   // }
-})
+});
 
 export const api = apiAdminInstance;
 
 apiAdminInstance.interceptors.request.use(
-  async config => {
-    const token = localStorage.getItem('auth_token');
+  async (config) => {
+    const token = localStorage.getItem("auth_token");
     // const token = typeof window !== 'undefined' ? localStorage.getItem('auth_token') : null
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    config.headers['ngrok-skip-browser-warning'] = 'true'
+    config.headers["ngrok-skip-browser-warning"] = "true";
     return config;
   },
-  error => Promise.reject(error)
+  (error) => Promise.reject(error)
 );
 
 apiAdminInstance.interceptors.response.use(
   function (response) {
     return response;
   },
-  error => {
+  (error) => {
     const { response } = error;
-  
+
     if (response.status === 401) {
-      localStorage.removeItem('auth_token');
-      window.location.href = '/login';
+      localStorage.removeItem("auth_token");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
